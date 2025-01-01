5ce852a4856d71633dd42bbca336b111057fa485
Your Name <you@example.com>
2025-01-01 08:18:48 +0100

    google tag manager script added to SEO.tsx

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -33,6 +33,15 @@ const SEO = ({ title, description, keywords, logoUrl, author, siteUrl }: SEOProp
   return (
     <>
       <head>
+        {/* Google tag manager script */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Y4J28FM147"></Script> 
+      <Script id="google-analytics"> 
+          {` window.dataLayer = window.dataLayer || []; 
+          function gtag(){dataLayer.push(arguments);} 
+          gtag('js', new Date()); gtag('config', 'G-Y4J28FM147'); `} 
+      </Script>
+
+
         {/* Basic Meta Tags */}
         <title>{title}</title>
         <meta charSet="UTF-8" />
