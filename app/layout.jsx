33776926f0c42e7813fcc3fe320cbd35e77a5a02
Your Name <you@example.com>
2025-01-01 07:52:24 +0100

    google tag manager script added to layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,6 +8,7 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import CryptoPriceTicker from '@/components/crypto-prices';
 import SEO from '@/components/SEO';
+import Script from 'next/script';
 
 export default function RootLayout({ children }) {
     // Create a new QueryClient instance for each request
@@ -16,6 +17,12 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" data-theme="lofi">
             <head>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Y4J28FM147"></Script> 
+                <Script id="google-analytics"> 
+                    {` window.dataLayer = window.dataLayer || []; 
+                    function gtag(){dataLayer.push(arguments);} 
+                    gtag('js', new Date()); gtag('config', 'G-Y4J28FM147'); `} 
+                </Script>
                 <link rel="manifest" href="/manifest.json" />
                 <link rel="icon" href="/favicon.ico" sizes="any" />
             </head>
